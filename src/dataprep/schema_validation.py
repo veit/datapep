dd8f372fd7d25184ed731f056ce00b99b6cb6359
Veit  Schiele <veit@cusy.io>
2026-06-17 17:54:13 +0200

    Verify schema of a new dataframe

--- src/dataprep/schema_validation.py
+++ src/dataprep/schema_validation.py
@@ -12,3 +12,17 @@ def discover_schema():
     constrains = discover_df(refernce_df)
     with Path.open("iot_example.json", "w") as f:
         f.write(constrains.to_json())
+
+
+new_df = pd.read_csv(
+    "https://raw.githubusercontent.com/kjam/data-cleaning-101/master/data/iot_example_with_nulls.csv"
+)
+
+def verify_new_df():
+    verified = verify_df(new_df, "iot_example.json")
+    print(str(verified))
+
+def show_invalid_records():
+    detected = detect_df(new_df, "iot_example.json")
+    detected_index = detected.detected().index
+    return new_df[new_df.index.isin(detected_index)]
