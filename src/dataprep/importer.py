dfac8d20a6b150bc9cb077507c2a44b75a798ad5
Veit  Schiele <veit@cusy.io>
2026-06-17 12:04:48 +0200

    check_shape-Funktion hinzugefügt

--- src/dataprep/importer.py
+++ src/dataprep/importer.py
@@ -12,19 +12,19 @@ new_customers = pd.read_csv(
     encoding="utf-8",
 )
 
-customers_shape = customers.shape
 
-if customers_shape[0] == 0:
-    print("Der DataFrame enthält keine Datensätze")
 
-if customers_shape[1] != 8:
-    print("WARNUNG: Der DataFrame enthält nicht die erwarteten acht Spalten")
-
-if customers_shape[1] == 8:
-    print("Der DataFrame enthält die erwarteten acht Spalten")
+def check_shape(df):
+    df_shape = df.shape
+    if df_shape[0] == 0:
+        print("Der DataFrame enthält keine Datensätze")
+    if df_shape[1] != 8:
+        print("WARNUNG: Der DataFrame enthält nicht die erwarteten acht Spalten")
+    if df_shape[1] == 8:
+      print("Der DataFrame enthält die erwarteten acht Spalten")
 
 def add_data(existing_df, new_df):
-    """Zusammenführen von Datensätzen
+    """Zusammenführen von zwei Datensätzen
 
     >>> from dataprep import importer
     >>> importer.add_data(importer.customers, importer.new_customers)
